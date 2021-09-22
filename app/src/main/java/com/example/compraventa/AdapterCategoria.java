56277cf649afc88ca138465ec8627bc9bbd7b594
brunoagretti <brunoagretti@hotmail.com>
2021-09-22 16:47:30 -0300

    Listener en el recycler

--- app/src/main/java/com/example/compraventa/AdapterCategoria.java
+++ app/src/main/java/com/example/compraventa/AdapterCategoria.java
@@ -1,11 +1,14 @@
 package com.example.compraventa;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -37,16 +40,30 @@ public class AdapterCategoria extends RecyclerView.Adapter<AdapterCategoria.View
 
         TextView textoCategoria;
         ImageView imagenCategoria;
+        LinearLayout layoutPadre;
 
         public ViewHolderCategoria(View itemView) {
             super(itemView);
+
+            layoutPadre = itemView.findViewById(R.id.layoutHorizontalID);
+
+
             textoCategoria = itemView.findViewById(R.id.textoCategoriaID);
             imagenCategoria = itemView.findViewById(R.id.imagenCategoriaID);
+
         }
 
         public void asignarDatos(CategoriaVo c) {
             textoCategoria.setText(c.getNombre());
             imagenCategoria.setImageResource(c.getImagen());
+
+            layoutPadre.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("PRESIONAR","Presionó "+c.getNombre());
+                }
+            });
+
         }
     }
 }
